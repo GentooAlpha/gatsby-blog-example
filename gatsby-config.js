be5ebb18d7b39907a97ddaef46f1e4b2a01410ca
Michael Guzman <michael@twentypenguins.com>
2020-01-04 22:22:16 -0500

    Netlify mods

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,6 +13,11 @@ module.exports = {
         name: 'files',
         path: `${__dirname}/src/markdown`,
       },
+
+      resolve: `gatsby-plugin-netlify-identity`,
+      options: {
+        url: `https://vigilant-shockley-279aaf.netlify.com/` // required!
+      },
     },
 
     'gatsby-transformer-remark'
